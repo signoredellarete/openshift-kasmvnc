7568d30f72859cb26e421082ff55112e32070c7b
Fabrizio Verde <37313214+signoredellarete@users.noreply.github.com>
2024-10-15 15:22:48 +0200

    Config script

--- mod-obsidian/config-script.sh
+++ mod-obsidian/config-script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 /usr/bin/xfce4-panel --add clock
+
+# Create an empty Obsidian Vault
+mkdir -p /home/kasm-user/obsidian-vault && \
+mkdir -p /home/kasm-user/.config/obsidian && \
+echo '{"vaults":{"'$(openssl rand -hex 8)'":{"path":"/home/kasm-user/obsidian-vault","ts":'$(shuf -i 1000000000000-9999999999999 -n 1)',"open":true}}}' > .config/obsidian/obsidian.json
