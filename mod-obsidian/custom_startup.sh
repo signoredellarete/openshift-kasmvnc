c04148f40f2177628d18724f52f28d3bf8ff76eb
Fabrizio Verde <37313214+signoredellarete@users.noreply.github.com>
2024-10-16 01:04:21 +0200

    custom_startup.sh only

---
+++ mod-obsidian/custom_startup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# VARS
+now=`date +%Y-%m-%d_%H-%M-%S`
+remote_repo="https://github.com/signoredellarete/openshift-kasmvnc.git"
+git_dir='/tmp/custom'
+git_custom_tasks='/tmp/custom/mod-obsidian/custom-tasks.sh'
+local_custom_tasks='/tmp/custom/mod-obsidian/local-custom-tasks.sh'
+local_conf_backup_file='/tmp/custom/mod-obsidian/local-custom-tasks.sh.'${now}
+custom_tasks_log='/tmp/custom/mod-obsidian/local-custom-tasks.log'
+
+
+# BINS
+git='/usr/bin/git'
+diff='/usr/bin/diff'
+cp='/usr/bin/cp'
+
+
+# FUNCTIONS
+clone_remote_repo(){
+  if [ ! -d ${git_dir} ];then
+    #${git} clone ${remote_repo} ${git_dir}
+  fi
+}
+
+update_local_repo(){
+  cd ${git_dir}
+  ${git} pull > /dev/null 2>&1
+}
+
+exec_local_tasks(){
+  nohup ${local_custom_tasks} >> ${custom_tasks_log} 2>&1 &
+}
+
+is_different(){
+  ${diff} ${local_custom_tasks} ${git_custom_tasks} > /dev/null 2>&1
+  echo $?
+}
+
+backup_custom_tasks(){
+  ${cp} ${local_custom_tasks} ${local_conf_backup_file}
+}
+
+update_local_custom_tasks(){
+  ${cp} ${git_custom_tasks} ${local_custom_tasks}
+  chmod +x ${local_custom_tasks}
+}
+
+
+# EXEC
+echo $current_date" - /dockerstartup/custom_startup.sh started"
+clone_remote_repo
+update_local_custom_tasks
+exec_local_tasks
+
+while
+  true
+do
+  update_local_repo
+  if [ $(is_different) -eq 1 ];then
+    echo ${now} "Remote file is different! Local customuration will be updated."
+    backup_custom_tasks
+    update_local_custom_tasks
+    exec_local_tasks
+  fi
+  sleep 60
+done
