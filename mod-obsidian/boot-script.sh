10b68c98c03f89b6b5174ac1e8894a39652d9747
Fabrizio Verde <37313214+signoredellarete@users.noreply.github.com>
2024-10-15 18:36:42 +0200

    boot-script

--- mod-obsidian/boot-script.sh
+++ mod-obsidian/boot-script.sh
@@ -3,4 +3,5 @@ current_date=$(date)
 echo $current_date" - boot-script.sh started" >> /home/kasm-user/boot-script.log
 rm -rf /tmp/config
 git clone https://github.com/signoredellarete/openshift-kasmvnc.git /tmp/config
+chmod +x /tmp/config/mod-obsidian/config-script.sh
 nohup /tmp/config/mod-obsidian/config-script.sh >> /tmp/config/mod-obsidian/config-script.log 2>&1 &
